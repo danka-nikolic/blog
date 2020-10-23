6f6f92930d1a8f75b83512ad78af0c33396cbcb5
Hermanitas\Hermanitas <dankanikolic90@gmail.com>
2020-10-23 15:21:13 +0200

    [blog-list] :: Replace blog list of strings with Blog list model

--- blog-web-client/src/app/blog-list-item/blog-list-item.component.ts
+++ blog-web-client/src/app/blog-list-item/blog-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Blog } from './model/blog.model';
 
 @Component({
   selector: 'app-blog-list-item',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogListItemComponent implements OnInit {
 
-  title = '';
+  @Input() blog: Blog;
   
   constructor() { }
 
