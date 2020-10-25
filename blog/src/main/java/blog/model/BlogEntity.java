47929e0c361edfa6a9ff90a98634337ec559466b
Hermanitas\Hermanitas <dankanikolic90@gmail.com>
2020-10-25 12:43:49 +0100

    [feature/load-data-on-app-startup] :: Change blog entity content field type from varchar 255 to CLOB

--- blog/src/main/java/blog/model/BlogEntity.java
+++ blog/src/main/java/blog/model/BlogEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -26,6 +27,7 @@ public class BlogEntity {
     private Long id;
 	private LocalDate date;
 	private String title;
+	@Lob
     private String content;
 	
     public BlogEntity() {}
